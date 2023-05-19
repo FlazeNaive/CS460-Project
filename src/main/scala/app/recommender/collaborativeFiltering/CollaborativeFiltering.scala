f179ea78598fa8f40276bf83dfd487acd8819c7b
FlazeNaive <603015257@qq.com>
2023-05-19 17:22:00 +0200

    implemented vanilla CollaborativeFiltering

--- src/main/scala/app/recommender/collaborativeFiltering/CollaborativeFiltering.scala
+++ src/main/scala/app/recommender/collaborativeFiltering/CollaborativeFiltering.scala
@@ -1,7 +1,9 @@
 package app.recommender.collaborativeFiltering
 
-
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.recommendation.ALS
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
+import org.apache.spark.mllib.recommendation.Rating
 
 class CollaborativeFiltering(rank: Int,
                              regularizationParameter: Double,
@@ -10,10 +12,34 @@ class CollaborativeFiltering(rank: Int,
 
   // NOTE: set the parameters according to the project description to get reproducible (deterministic) results.
   private val maxIterations = 20
-  private var model = null
+  private var model: MatrixFactorizationModel  = null
 
-  def init(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): Unit = ???
+  def init(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): Unit = {
+//     val newALS = new ALS()
+//     newALS.setMaxIter(maxIterations)
+//     newALS.setNumBlocks(n_parallel)
+//     newALS.setRank(rank)
+//     newALS.setRegParam(regularizationParameter)
+//     newALS.setSeed(seed)
 
-  def predict(userId: Int, movieId: Int): Double = ???
+    val ratingUserMovie = ratingsRDD.groupBy(_._1)
+      .flatMapValues(x => {
+        val moviesRating = x.groupBy(_._2).mapValues(x => x.toList
+          .maxBy(_._5)
+          ._4)
+          .toList
+        moviesRating
+      })
+
+    val ratings = ratingUserMovie.map(x => Rating(x._1, x._2._1, x._2._2))
+
+    model = ALS.train(ratings,
+                      rank,
+                      maxIterations,
+                      regularizationParameter)
+  }
+  def predict(userId: Int, movieId: Int): Double = {
+    model.predict(userId, movieId)
+  }
 
 }
